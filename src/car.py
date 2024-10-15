6b70b48b981d601240abb5db3700b4c101a9cbf3
Torin Etheridge <torinriley220@gmail.com>
2024-10-15 07:44:13 -0400

    update

--- src/car.py
+++ src/car.py
@@ -8,20 +8,17 @@ class Car:
         self.y = y
         self.width = width
         self.height = height
-        self.heading = 0  # direction in radians
-        self.total_reward = 0  # Track total rewards
+        self.heading = 0 
+        self.total_reward = 0  
 
     def move(self, steering_angle, speed):
-        # Update the heading based on steering angle
         self.heading += steering_angle
 
-        # Move the car forward in the direction of the heading
         self.x += speed * np.cos(self.heading)
         self.y += speed * np.sin(self.heading)
 
     def draw(self, frame):
-        # Draw the car as a rectangle on the frame
-        car_color = (255, 0, 0)  # Blue car
+        car_color = (255, 0, 0)  
         cv2.rectangle(frame, 
                       (int(self.x), int(self.y)), 
                       (int(self.x + self.width), int(self.y + self.height)), 
